edddee3ced7e4d0eac669e88adeb2d09038577ba
ayibogan17 <fromyazan@gmail.com>
2025-12-15 17:53:00 +0300

    set node runtime for uploads route

--- app/api/uploads/route.ts
+++ app/api/uploads/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { NextResponse } from "next/server";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
